f65456628645dab746013059a742ea4c20a491e9
Houjuu-Nue <usami-ssc@protonmail.com>
2019-04-05 11:29:33 +0800

    :books: Add docs for Descriptor*CI.

--- base/src/ci/descriptor.rs
+++ base/src/ci/descriptor.rs
@@ -1,3 +1,4 @@
+//! Types which simplify the creation of Vulkan descriptor objects.
 
 use ash::vk;
 use ash::version::DeviceV1_0;
@@ -11,7 +12,23 @@ use crate::vkuint;
 use std::ptr;
 
 // ----------------------------------------------------------------------------------------------
-/// Wrapper class for vk::DescriptorPoolCreateInfo.
+/// Wrapper class for `vk::DescriptorPoolCreateInfo`.
+///
+///
+/// The default values are defined as follows:
+/// ``` ignore
+/// vk::DescriptorPoolCreateInfo {
+///     s_type: vk::StructureType::DESCRIPTOR_POOL_CREATE_INFO,
+///     p_next: ptr::null(),
+///     flags : vk::DescriptorPoolCreateFlags::empty(),
+///     max_sets: 0,
+///     pool_size_count: 0,
+///     p_pool_sizes   : ptr::null(),
+/// }
+/// ```
+///
+/// See [VkDescriptorPoolCreateInfo](https://www.khronos.org/registry/vulkan/specs/1.1-extensions/man/html/VkDescriptorPoolCreateInfo.html) for more detail.
+///
 #[derive(Debug, Clone)]
 pub struct DescriptorPoolCI {
 
@@ -44,7 +61,10 @@ impl AsRef<vk::DescriptorPoolCreateInfo> for DescriptorPoolCI {
 impl VkObjectBuildableCI for DescriptorPoolCI {
     type ObjectType = vk::DescriptorPool;
 
+    /// Create `vk::DescriptorPool` object, and return its handle.
     fn build(&self, device: &VkDevice) -> VkResult<Self::ObjectType> {
+
+        debug_assert!(!self.pool_sizes.empty(), "The count of pool sizes must be greater than 0!");
 
         let descriptor_pool = unsafe {
             device.logic.handle.create_descriptor_pool(self.as_ref(), None)
@@ -56,7 +76,12 @@ impl VkObjectBuildableCI for DescriptorPoolCI {
 
 impl DescriptorPoolCI {
 
+    /// Initialize `vk::DescriptorPoolCreateInfo` with default value.
+    ///
+    /// `max_set_count` is the maximum number of descriptor sets that this descriptor pool may allocated.
     pub fn new(max_set_count: vkuint) -> DescriptorPoolCI {
+
+        debug_assert!(max_set_count > 0, "max_set_count must be greater than 0!");
 
         DescriptorPoolCI {
             inner: vk::DescriptorPoolCreateInfo {
@@ -67,16 +92,26 @@ impl DescriptorPoolCI {
         }
     }
 
+    /// Set the `flags` member for `vk::DescriptorPoolCreateInfo`.
+    ///
+    /// It specifies the some supported operations for this pool.
     #[inline(always)]
     pub fn flags(mut self, flags: vk::DescriptorPoolCreateFlags) -> DescriptorPoolCI {
         self.inner.flags = flags; self
     }
 
+    /// Add a new descriptor type that can be allocated by this pool.
+    ///
+    /// `type_` is the type of descriptor.
+    ///
+    /// `count` is tha maximum number of this descriptor that can be allocated by this pool.
     #[inline]
-    pub fn add_descriptor(mut self, r#type: vk::DescriptorType, count: vkuint) -> DescriptorPoolCI {
+    pub fn add_descriptor(mut self, type_: vk::DescriptorType, count: vkuint) -> DescriptorPoolCI {
+
+        debug_assert!(count > 0, "The count of descriptor must be greater than 0!");
 
         self.pool_sizes.push(vk::DescriptorPoolSize {
-            ty: r#type,
+            ty: type_,
             descriptor_count: count,
         });
         self.inner.pool_size_count = self.pool_sizes.len() as _;
@@ -96,7 +131,21 @@ impl VkObjectDiscardable for vk::DescriptorPool {
 
 
 // ----------------------------------------------------------------------------------------------
-/// Wrapper class for vk::DescriptorSetLayoutCreateInfo.
+/// Wrapper class for `vk::DescriptorSetLayoutCreateInfo`.
+///
+/// The default values are defined as follows:
+/// ``` ignore
+/// vk::DescriptorSetLayoutCreateInfo {
+///     s_type: vk::StructureType::DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
+///     p_next: ptr::null(),
+///     flags: vk::DescriptorSetLayoutCreateFlags::empty(),
+///     binding_count: 0,
+///     p_bindings   : ptr::null(),
+/// }
+/// ```
+///
+/// See [VkDescriptorSetLayoutCreateInfo](https://www.khronos.org/registry/vulkan/specs/1.1-extensions/man/html/VkDescriptorSetLayoutCreateInfo.html) for more detail.
+///
 #[derive(Debug, Clone)]
 pub struct DescriptorSetLayoutCI {
     inner: vk::DescriptorSetLayoutCreateInfo,
@@ -127,6 +176,7 @@ impl AsRef<vk::DescriptorSetLayoutCreateInfo> for DescriptorSetLayoutCI {
 impl VkObjectBuildableCI for DescriptorSetLayoutCI {
     type ObjectType = vk::DescriptorSetLayout;
 
+    /// Create `vk::DescriptorSetLayout` object, and return its handle.
     fn build(&self, device: &VkDevice) -> VkResult<Self::ObjectType> {
 
         let descriptor_set_layout = unsafe {
@@ -139,6 +189,8 @@ impl VkObjectBuildableCI for DescriptorSetLayoutCI {
 
 impl DescriptorSetLayoutCI {
 
+    /// Initialize `vk::DescriptorSetLayoutCreateInfo` with default value.
+    #[inline(always)]
     pub fn new() -> DescriptorSetLayoutCI {
 
         DescriptorSetLayoutCI {
@@ -147,6 +199,7 @@ impl DescriptorSetLayoutCI {
         }
     }
 
+    /// Add set layout bindings to this descriptor set.
     #[inline(always)]
     pub fn add_binding(mut self, binding: vk::DescriptorSetLayoutBinding) -> DescriptorSetLayoutCI {
 
@@ -155,6 +208,9 @@ impl DescriptorSetLayoutCI {
         self.inner.p_bindings    = self.bindings.as_ptr(); self
     }
 
+    /// Set the `flags` member for `vk::DescriptorSetLayoutCreateInfo`.
+    ///
+    /// It specifies options for descriptor set layout creation.
     #[inline(always)]
     pub fn flags(mut self, flags: vk::DescriptorSetLayoutCreateFlags) -> DescriptorSetLayoutCI {
         self.inner.flags = flags; self
@@ -172,7 +228,21 @@ impl VkObjectDiscardable for vk::DescriptorSetLayout {
 // ----------------------------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------------------------
-/// Wrapper class for vk::DescriptorSetAllocateInfo.
+/// Wrapper class for `vk::DescriptorSetAllocateInfo`.
+///
+/// The default values are defined as follows:
+/// ``` ignore
+/// vk::DescriptorSetAllocateInfo {
+///     s_type: vk::StructureType::DESCRIPTOR_SET_ALLOCATE_INFO,
+///     p_next: ptr::null(),
+///     descriptor_pool: vk::DescriptorPool::null(),
+///     descriptor_set_count: 0,
+///     p_set_layouts       : ptr::null(),
+/// }
+/// ```
+///
+/// See [VkDescriptorSetAllocateInfo](https://www.khronos.org/registry/vulkan/specs/1.1-extensions/man/html/VkDescriptorSetAllocateInfo.html) for more detail.
+///
 #[derive(Debug, Clone)]
 pub struct DescriptorSetAI {
     inner: vk::DescriptorSetAllocateInfo,
@@ -203,7 +273,10 @@ impl AsRef<vk::DescriptorSetAllocateInfo> for DescriptorSetAI {
 impl VkObjectBuildableCI for DescriptorSetAI {
     type ObjectType = Vec<vk::DescriptorSet>;
 
+    /// Create `vk::DescriptorSet` objects, and return their handles.
     fn build(&self, device: &VkDevice) -> VkResult<Self::ObjectType> {
+
+        debug_assert!(!self.set_layouts.empty(), "Descriptor sets count must be greater than 0!");
 
         let descriptor_sets = unsafe {
             device.logic.handle.allocate_descriptor_sets(self.as_ref())
@@ -215,6 +288,9 @@ impl VkObjectBuildableCI for DescriptorSetAI {
 
 impl DescriptorSetAI {
 
+    /// Initialize `vk::DescriptorSetAllocateInfo` with default value.
+    ///
+    /// `pool` is the pool where these sets will be allocated from.
     pub fn new(pool: vk::DescriptorPool) -> DescriptorSetAI {
 
         DescriptorSetAI {
@@ -226,6 +302,7 @@ impl DescriptorSetAI {
         }
     }
 
+    /// Add a new descriptor set layout to this allocations.
     #[inline(always)]
     pub fn add_set_layout(mut self, set_layout: vk::DescriptorSetLayout) -> DescriptorSetAI {
 
@@ -257,7 +334,26 @@ impl VkObjectAllocatable for &[vk::DescriptorSet] {
 // ----------------------------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------------------------
-/// Wrapper class for vk::WriteDescriptorSet(for vk::Buffer).
+/// Wrapper class for `vk::WriteDescriptorSet`(for `vk::Buffer`).
+///
+/// The default values are defined as follows:
+/// ``` ignore
+/// vk::WriteDescriptorSet {
+///     s_type: vk::StructureType::WRITE_DESCRIPTOR_SET,
+///     p_next: ptr::null(),
+///     dst_set: vk::DescriptorSet::null(),
+///     dst_binding: 0,
+///     dst_array_element   : 0,
+///     descriptor_count    : 0,
+///     descriptor_type     : vk::DescriptorType::UNIFORM_BUFFER,
+///     p_image_info        : ptr::null(),
+///     p_buffer_info       : ptr::null(),
+///     p_texel_buffer_view : ptr::null(),
+/// }
+/// ```
+///
+/// See [VkWriteDescriptorSet](https://www.khronos.org/registry/vulkan/specs/1.1-extensions/man/html/VkWriteDescriptorSet.html) for more detail.
+///
 #[derive(Debug, Clone)]
 pub struct DescriptorBufferSetWI {
 
@@ -293,19 +389,27 @@ impl AsRef<vk::WriteDescriptorSet> for DescriptorBufferSetWI {
 
 impl DescriptorBufferSetWI {
 
-    pub fn new(set: vk::DescriptorSet, binding: vkuint, r#type: vk::DescriptorType) -> DescriptorBufferSetWI {
+    /// Initialize `vk::WriteDescriptorSet` with default value.
+    ///
+    /// `set` is the destination descriptor set to update.
+    ///
+    /// `bindings` is the descriptor binding within the set.
+    ///
+    /// `type_` specifies the type of buffer descriptor to update.
+    pub fn new(set: vk::DescriptorSet, binding: vkuint, type_: vk::DescriptorType) -> DescriptorBufferSetWI {
 
         DescriptorBufferSetWI {
             inner: vk::WriteDescriptorSet {
                 dst_set: set,
                 dst_binding: binding,
-                descriptor_type: r#type,
+                descriptor_type: type_,
                 ..DescriptorBufferSetWI::default_ci()
             },
             writes: Vec::new(),
         }
     }
 
+    /// Add a new buffer descriptor to update for the set.
     #[inline(always)]
     pub fn add_buffer(mut self, info: vk::DescriptorBufferInfo) -> DescriptorBufferSetWI {
 
@@ -314,6 +418,7 @@ impl DescriptorBufferSetWI {
         self.inner.p_buffer_info    = self.writes.as_ptr(); self
     }
 
+    /// Reset all buffer descriptors to update for the set.
     #[inline(always)]
     pub fn set_buffer(&mut self, infos: Vec<vk::DescriptorBufferInfo>) {
 
@@ -323,6 +428,9 @@ impl DescriptorBufferSetWI {
         self.writes = infos;
     }
 
+    /// Set the `array_element` member for `vk::WriteDescriptorSet`.
+    ///
+    /// It is the starting element index in the descriptor array.
     #[inline(always)]
     pub fn dst_array_element(mut self, array_element: vkuint) -> DescriptorBufferSetWI {
         self.inner.dst_array_element = array_element; self
@@ -331,7 +439,26 @@ impl DescriptorBufferSetWI {
 // ----------------------------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------------------------
-/// Wrapper class for vk::WriteDescriptorSet(for vk::Image).
+/// Wrapper class for `vk::WriteDescriptorSet`(for `vk::Image`).
+///
+/// The default values are defined as follows:
+/// ``` ignore
+/// vk::WriteDescriptorSet {
+///     s_type: vk::StructureType::WRITE_DESCRIPTOR_SET,
+///     p_next: ptr::null(),
+///     dst_set: vk::DescriptorSet::null(),
+///     dst_binding: 0,
+///     dst_array_element   : 0,
+///     descriptor_count    : 0,
+///     descriptor_type     : vk::DescriptorType::UNIFORM_BUFFER,
+///     p_image_info        : ptr::null(),
+///     p_buffer_info       : ptr::null(),
+///     p_texel_buffer_view : ptr::null(),
+/// }
+/// ```
+///
+/// See [VkWriteDescriptorSet](https://www.khronos.org/registry/vulkan/specs/1.1-extensions/man/html/VkWriteDescriptorSet.html) for more detail.
+///
 #[derive(Debug, Clone)]
 pub struct DescriptorImageSetWI {
 
@@ -367,19 +494,27 @@ impl AsRef<vk::WriteDescriptorSet> for DescriptorImageSetWI {
 
 impl DescriptorImageSetWI {
 
-    pub fn new(set: vk::DescriptorSet, binding: vkuint, r#type: vk::DescriptorType) -> DescriptorImageSetWI {
+    /// Initialize `vk::WriteDescriptorSet` with default value.
+    ///
+    /// `set` is the destination descriptor set to update.
+    ///
+    /// `bindings` is the descriptor binding within the set.
+    ///
+    /// `type_` specifies the type of image descriptor to update.
+    pub fn new(set: vk::DescriptorSet, binding: vkuint, type_: vk::DescriptorType) -> DescriptorImageSetWI {
 
         DescriptorImageSetWI {
             inner: vk::WriteDescriptorSet {
                 dst_set: set,
                 dst_binding: binding,
-                descriptor_type: r#type,
+                descriptor_type: type_,
                 ..DescriptorImageSetWI::default_ci()
             },
             writes: Vec::new(),
         }
     }
 
+    /// Add a new image descriptor to update for the set.
     #[inline(always)]
     pub fn add_image(mut self, info: vk::DescriptorImageInfo) -> DescriptorImageSetWI {
 
@@ -388,6 +523,7 @@ impl DescriptorImageSetWI {
         self.inner.p_image_info     = self.writes.as_ptr(); self
     }
 
+    /// Reset all image descriptors to update for the set.
     #[inline(always)]
     pub fn set_images(&mut self, infos: Vec<vk::DescriptorImageInfo>) {
 
@@ -397,6 +533,9 @@ impl DescriptorImageSetWI {
         self.writes = infos;
     }
 
+    /// Set the `array_element` member for `vk::WriteDescriptorSet`.
+    ///
+    /// It is the starting element index in the descriptor array.
     #[inline(always)]
     pub fn dst_array_element(mut self, array_element: vkuint) -> DescriptorImageSetWI {
         self.inner.dst_array_element = array_element; self
@@ -405,7 +544,7 @@ impl DescriptorImageSetWI {
 // ----------------------------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------------------------
-/// Convenient struct to update descriptor set.
+/// Utility type to update descriptor set.
 #[derive(Default)]
 pub struct DescriptorSetsUpdateCI<'a> {
 
@@ -422,11 +561,13 @@ impl DescriptorSetWritable for DescriptorImageSetWI  {}
 
 impl<'a, 'b: 'a> DescriptorSetsUpdateCI<'a> {
 
+    /// Constructor of `DescriptorSetsUpdateCI`.
     #[inline(always)]
     pub fn new() -> DescriptorSetsUpdateCI<'a> {
         DescriptorSetsUpdateCI::default()
     }
 
+    /// Add a `DescriptorBufferSetWI` or `DescriptorImageSetWI` to the descriptor update sequences.
     #[inline(always)]
     pub fn add_write(mut self, value: &'b impl DescriptorSetWritable) -> DescriptorSetsUpdateCI<'a> {
         self.writes.push(value.as_ref().clone()); self
@@ -437,6 +578,7 @@ impl<'a, 'b: 'a> DescriptorSetsUpdateCI<'a> {
         self.copies.push(value.clone()); self
     }
 
+    /// Execute the descriptor sets update operations.
     #[inline(always)]
     pub fn update(self, device: &VkDevice) {
 
